bc94a08bc2b3979a84411ed90353b301cea4ebbc
Sujal Tangde <sujaltagade01@gmail.com>
2024-09-06 19:43:56 +0530

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-
+import './App.css'
 function App() {
 
   return (
-    <>
+    <div className="">
       
       <div id="ecowiser-verified" data-url="https://wiser.eco/brands/category/Clothing%20and%20Accessories/557" 
       data-badges="Vegan, Low Waste, Low Carbon, Gives Back, Non Toxic, Promotes Circularity, Ethical Labour"></div>
 
-    </>
+    </div>
   )
 }
 
